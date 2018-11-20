01139370e61a88da66332d92880edf59bd0e7b88
Hugh Guiney <hugh@hughguiney.com>
2018-11-19 20:16:08 -0600

    More efficient colorway switcher?

--- src/script/products.js
+++ src/script/products.js
@@ -1,40 +1,21 @@
 (function () {
   "use strict";
 
-  function getSrcset( src ) {
-    // src = redblue-tokyo/product--natural.png
-    var srcParts = src.split( '.' );
-    return src + ' 1x, ' + srcParts[0] + '@2x.' + srcParts[1] + ' 2x, ' + srcParts[0] + '@3x.' + srcParts[1] + ' 3x';
-  }
-
   function changeProductShot( event ) {
     var $clicked = event.target;
-    var $img = $clicked;
-    var colorway;
-    var src;
-    var newSrc;
-    var newColorway;
 
     if ( ( $clicked.nodeName.toLowerCase() === 'button' ) && ( $clicked.getAttribute( 'class' ).match( /\bcolor\b/i ) !== null ) ) {
-      newColorway = $clicked.textContent.toLowerCase().replace( /\s+/, '-' );
+      var colorway = $clicked.getAttribute( 'data-colorway' );
+      var product = colorway.split( '--' ); product = product[0];
+      var colorways = document.getElementById( product ).querySelectorAll( 'img' );
 
-      while ( !$img.hasAttribute( 'class' ) || $img.getAttribute( 'class' ).match( /\bproduct-colors\b/i ) === null ) {
-        $img = $img.parentNode;
-      }
-
-      while ( !$img.hasAttribute( 'class' ) || $img.getAttribute( 'class' ).match( /\bproduct-link\b/i ) === null ) {
-        $img = $img.previousElementSibling;
-      }
-
-      $img = $img.querySelector( 'img' );
-      src = $img.getAttribute( 'src' );
-      colorway = $img.getAttribute( 'data-colorway' );
-
-      if ( colorway !== newColorway ) {
-        newSrc = src.replace( colorway, newColorway );
-        $img.setAttribute( 'data-colorway', newColorway );
-        $img.setAttribute( 'srcset', getSrcset( newSrc ) );
-        $img.setAttribute( 'src', newSrc );
+      for ( var i = 0; i < colorways.length; i++ ) {
+        var current = colorways[i];
+        if ( current.id === colorway ) {
+          current.hidden = false;
+        } else {
+          current.hidden = true;
+        }
       }
     }
   }
