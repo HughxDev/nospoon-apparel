85149e008a08ad4f59d159a5815db02237534542
Hugh Guiney <hugh@hughguiney.com>
2018-11-21 21:46:19 -0600

    Dynamic product page

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "hash_dot"
+gem "money"
--- site.rb
+++ site.rb
@@ -1,10 +1,19 @@
 require 'sinatra/base'
 require 'yaml'
 require 'hash_dot'
+require 'money'
 
 Hash.use_dot_syntax = true
+Money.locale_backend = :i18n
+I18n.config.available_locales = :en
+I18n.locale = :en
 
 class NoSpoonApparel < Sinatra::Base
+  # https://stackoverflow.com/a/37891309/214325
+  def strip_trailing_zero(n)
+    n.to_s.sub(/\.?0+$/, '')
+  end
+
   get "/" do
     erb :index
   end
@@ -25,8 +34,11 @@ class NoSpoonApparel < Sinatra::Base
     @data = YAML.load_file('data/products.yaml')[params['product']]
     @product = params['product']
     @default_colorway = @data['variants'][0]['colorways'][0]['id']
-    @default_colorway_src = "/collection/#{@product}/product--#{@default_colorway}.png"
-    @default_colorway_srcset = "#{@default_colorway_src} 1x, #{@default_colorway_src.sub '.png', '@2x.png'} 2x, #{@default_colorway_src.sub '.png', '@3x.png'} 3x"
+    @default_colorway_src = "/collection/#{@product}/product--#{@default_colorway}@2x.png"
+    # @default_colorway_srcset = "#{@default_colorway_src} 1x, #{@default_colorway_src.sub '.png', '@2x.png'} 2x, #{@default_colorway_src.sub '.png', '@3x.png'} 3x"
+    @default_price = @data.variants[0].price.USD.amount
+    @default_price_formatted = Money.from_amount(@default_price, "USD")
+    @default_price_clean = strip_trailing_zero(@default_price)
     erb :product
   end
 end
